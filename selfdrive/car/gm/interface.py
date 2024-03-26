03b3b5b3a44b774e648cf0b07549a0dbb9febf16
geniuth <geniuth@gmail.com>
2024-03-26 23:21:11 +0900

    저속 ki 값 원복

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -368,7 +368,7 @@ class CarInterface(CarInterfaceBase):
         ret.longitudinalTuning.kpBP = [5., 35.]
         ret.longitudinalTuning.kpV = [0.15, 0.21]
         ret.longitudinalTuning.kiBP = [0., 35.0]
-        ret.longitudinalTuning.kiV = [0.01, 0.01]
+        ret.longitudinalTuning.kiV = [0.08, 0.01]
         ret.longitudinalTuning.kf = 0.20
         ret.stoppingDecelRate = 0.8
       else:  # Pedal used for SNG, ACC for longitudinal control otherwise
