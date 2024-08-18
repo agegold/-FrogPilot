c6cb80d9c4a3efa007a813f3b086d13b00938625
agegold <66872114+agegold@users.noreply.github.com>
2024-08-18 10:51:38 +0900

    Update file_helpers.py

--- common/file_helpers.py
+++ common/file_helpers.py
@@ -25,7 +25,7 @@ class CallbackReader:
 
 @contextlib.contextmanager
 def atomic_write_in_dir(path: str, mode: str = 'w', buffering: int = -1, encoding: Optional[str] = None, newline: Optional[str] = None,
-                        overwrite: bool = False):
+                        overwrite: bool = True):
   """Write to a file atomically using a temporary file in the same directory as the destination file."""
   dir_name = os.path.dirname(path)
 
