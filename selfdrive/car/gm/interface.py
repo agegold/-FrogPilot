30db5381830b53ce917b550320d198b51c858b5d
agegold <66872114+agegold@users.noreply.github.com>
2024-12-28 17:39:10 +0900

    Update interface.py
          ret.minEnableSpeed = -1 * CV.MPH_TO_MS # carrot

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -155,7 +155,8 @@ class CarInterface(CarInterfaceBase):
       ret.radarUnavailable = RADAR_HEADER_MSG not in fingerprint[CanBus.OBSTACLE] and not docs
       ret.pcmCruise = False  # stock non-adaptive cruise control is kept off
       # supports stop and go, but initial engage must (conservatively) be above 18mph
-      ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      # ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      ret.minEnableSpeed = -1 * CV.MPH_TO_MS # carrot
       ret.minSteerSpeed = (6.7 if useEVTables else 7) * CV.MPH_TO_MS
 
       # Tuning
